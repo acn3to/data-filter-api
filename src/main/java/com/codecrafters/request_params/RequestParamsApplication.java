c1f7391f881e106578050d68dc5ee12b32862633
acn3to <costanetoow@gmail.com>
2024-08-21 21:15:16 -0300

    initial commit

---
+++ src/main/java/com/codecrafters/request_params/RequestParamsApplication.java
@@ -0,0 +1,13 @@
+package com.codecrafters.request_params;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestParamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestParamsApplication.class, args);
+	}
+
+}
